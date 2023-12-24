dae2d9c3c427733fa68694d1015acaf4ae1edd66
Pierre Etchemaite <pe-github@concept-micro.com>
2023-12-24 15:26:33 +0100

    Rename to look_and_say

--- bin/main.ml
+++ bin/main.ml
@@ -8,25 +8,30 @@ let rec seq_of_int n cont =
   else
     Seq.cons n cont
   
-let rec werber n =
+let rec look_and_say n =
   if n = 1 then Seq.return 1
   else
-    let prev = werber (n - 1) in
+    let prev = look_and_say (n - 1) in
     match prev () with
     | Seq.Nil -> Seq.empty
     | Seq.Cons (h, t) ->
        let rec aux current count seq =
          match seq () with
-         | Seq.Cons (h, t) when h = current -> aux current (count + 1) t
-         | Seq.Cons (h, t) -> seq_of_int count (fun () -> Seq.Cons (current, (aux h 1 t)))
-         | Seq.Nil -> seq_of_int count (Seq.return current) in
+         | Seq.Cons (h, t) when h = current ->
+            aux current (count + 1) t
+         | Seq.Cons (h, t) ->
+            (* need to use fun () -> Seq.Cons here instead of Seq.cons to delay computation. Meh *)
+            seq_of_int count (fun () -> Seq.Cons (current, (aux h 1 t)))
+         | Seq.Nil ->
+            seq_of_int count (Seq.return current) in
        aux h 1 t
+
+let print_digit =
+  let zero = Char.code '0' in
+  fun n -> print_char (Char.chr (zero + n))
 
 let () =
   let n = int_of_string Sys.argv.(1) in
-  let print_digit =
-    let zero = Char.code '0' in
-    fun n -> print_char (Char.chr (zero + n)) in
-  Seq.iter print_digit (werber n);
+  Seq.iter print_digit (look_and_say n);
   Printf.printf "%!"
     
